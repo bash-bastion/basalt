eca57d5adb0c2e6ff0d8327924d0648c22df0c56
Edwin Kofler <edwin@kofler.dev>
2021-08-04 13:42:58 -0700

    fix: Operating 'bpm.toml' context message shows once. Closes #68

--- pkg/lib/commands/do-add.sh
+++ pkg/lib/commands/do-add.sh
@@ -44,8 +44,7 @@ do-add() {
 			log.info "Adding all dependencies"
 
 			for pkg in "${REPLIES[@]}"; do
-				# TODO: cleanup
-				do-add "$pkg"
+				do-actual-add "$pkg" "$flag_ssh" "$flag_branch"
 			done
 		else
 			log.warn "No dependencies specified in 'dependencies' key"
@@ -59,45 +58,53 @@ do-add() {
 	fi
 
 	for repoSpec in "${pkgs[@]}"; do
-		if [[ -d "$repoSpec" && "${repoSpec::1}" == / ]]; then
-			die "Identifier '$repoSpec' is a directory, not a package"
-		fi
-
-		util.extract_data_from_input "$repoSpec" "$flag_ssh"
-		local uri="$REPLY1"
-		local site="$REPLY2"
-		local package="$REPLY3"
-		local ref="$REPLY4"
-
-		if [ "$site" = 'local' ]; then
-			die "Cannot install packages owned by username 'local' because that conflicts with linked packages"
-		fi
-
-		log.info "Adding '$repoSpec'"
-		do-plumbing-clone "$uri" "$site/$package" "$ref" "$flag_branch"
-		do-plumbing-add-deps "$site/$package"
-		do-plumbing-link-bins "$site/$package"
-		do-plumbing-link-completions "$site/$package"
-		do-plumbing-link-man "$site/$package"
-
-		# Install transitive dependencies
-		local subDep="$BPM_PACKAGES_PATH/$site/$package"
-
-		if [[ ! -d "$subDep" && -n "${BPM_MODE_TEST+x}" ]]; then
-			# During some tests, plumbing-* or Git commands may be stubbed,
-			# so the package may not actually be cloned
-			continue
-		fi
-
-		local oldWd="$PWD"
-		ensure.cd "$subDep"
-		local bpm_toml_file="$subDep/bpm.toml"
-		if [ -f "$bpm_toml_file" ]; then
-			if util.get_toml_array "$bpm_toml_file" 'dependencies'; then
-				do-add --all
-			fi
-		fi
-		ensure.cd "$oldWd"
-
+		do-actual-add "$repoSpec" "$flag_ssh" "$flag_branch"
 	done
+}
+
+do-actual-add() {
+	local repoSpec="$1"
+	local flag_ssh="$2"
+	local flag_branch="$3"
+
+	if [[ -d "$repoSpec" && "${repoSpec::1}" == / ]]; then
+		die "Identifier '$repoSpec' is a directory, not a package"
+	fi
+
+	util.extract_data_from_input "$repoSpec" "$flag_ssh"
+	local uri="$REPLY1"
+	local site="$REPLY2"
+	local package="$REPLY3"
+	local ref="$REPLY4"
+
+	if [ "$site" = 'local' ]; then
+		die "Cannot install packages owned by username 'local' because that conflicts with linked packages"
+	fi
+
+	log.info "Adding '$repoSpec'"
+	do-plumbing-clone "$uri" "$site/$package" "$ref" "$flag_branch"
+	do-plumbing-add-deps "$site/$package"
+	do-plumbing-link-bins "$site/$package"
+	do-plumbing-link-completions "$site/$package"
+	do-plumbing-link-man "$site/$package"
+
+	# TODO: install for packages.sh
+	# Install transitive dependencies
+	local subDep="$BPM_PACKAGES_PATH/$site/$package"
+
+	if [[ ! -d "$subDep" && -n "${BPM_MODE_TEST+x}" ]]; then
+		# During some tests, plumbing-* or Git commands may be stubbed,
+		# so the package may not actually be cloned
+		return
+	fi
+
+	local oldWd="$PWD"
+	ensure.cd "$subDep"
+	local bpm_toml_file="$subDep/bpm.toml"
+	if [ -f "$bpm_toml_file" ]; then
+		if util.get_toml_array "$bpm_toml_file" 'dependencies'; then
+			do-add --all
+		fi
+	fi
+	ensure.cd "$oldWd"
 }
