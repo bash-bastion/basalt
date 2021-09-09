61752c3e8bf8f8c20a3dd2d49da8b61af27061c3
Edwin Kofler <edwin@kofler.dev>
2021-09-08 18:30:04 -0700

    fix: Automatically prune out of date packages
    When installing packages, reconstruct the local symlinks every time.
    This ensures the directory always contains f r e s h and up to date
    symlinks (essentially autopruning without pruning)

--- pkg/lib/commands/do-install.sh
+++ pkg/lib/commands/do-install.sh
@@ -13,5 +13,10 @@ do-install() {
 		;;
 	esac done
 
+	# Everything in the local ./basalt_packages is a symlink to something in the global
+	# cellar directory. Thus, we can just remove it since it won't take long to re-symlink.
+	# This has the added benefit that outdated packages will automatically be pruned
+	rm -rf "${BASALT_LOCAL_PACKAGE_DIR:?}"
+
 	pkg.install_package "$BASALT_LOCAL_PROJECT_DIR"
 }
