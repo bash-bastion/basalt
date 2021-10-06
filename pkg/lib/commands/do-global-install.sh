7a88eb0ec7a27c1dbbb6a8816839f4941f75990f
Edwin Kofler <edwin@kofler.dev>
2021-10-06 08:39:10 -0700

    fix: Global installations work a gain

--- pkg/lib/commands/do-global-install.sh
+++ pkg/lib/commands/do-global-install.sh
@@ -7,20 +7,21 @@ do-global-install() {
 		newindent.die "No arguments or flags must be specified"
 	fi
 
-	# TODO: this should be changed when we make global location dot_basalt
 	if ! rm -rf "$BASALT_GLOBAL_DATA_DIR/global/.basalt"; then
 		print.indent-die "Could not remove global '.basalt' directory"
 	fi
 
-	local -a deps=()
+	local -a dependencies=()
 	local dep=
 	while IFS= read -r dep; do
 		if [ -z "$dep" ]; then
 			continue
 		fi
 
-		deps+=("$dep")
+		dependencies+=("$dep")
 	done < "$BASALT_GLOBAL_DATA_DIR/global/dependencies"; unset dep
 
-	pkg.install_package "$BASALT_GLOBAL_DATA_DIR/global" 'lenient' "${deps[@]}"
+	pkg.install_package "$BASALT_GLOBAL_DATA_DIR/global" 'lenient' "${dependencies[@]}"
+	pkg.phase_local_integration_recursive "$BASALT_GLOBAL_DATA_DIR/global" 'yes' 'strict' "${dependencies[@]}"
+	pkg.phase_local_integration_nonrecursive "$BASALT_GLOBAL_DATA_DIR/global"
 }
