fd67bc961087929c9ca81505511169103cbe9852
Edwin Kofler <edwin@kofler.dev>
2021-10-15 16:10:04 -0700

    fix: No longer required passing '--' to pass args for run

--- pkg/lib/commands/do-run.sh
+++ pkg/lib/commands/do-run.sh
@@ -3,36 +3,20 @@
 do-run() {
 	util.init_local
 
-	local -a args=()
-	for arg; do case "$arg" in
-	--)
-		shift
-		break
-		;;
-	-*)
-		bprint.die "Flag '$arg' not recognized"
-		;;
-	*)
-		args+=("$arg")
-		shift
-		;;
-	esac done
-
-	if ((${#args[@]} == 0)); then
+	if (($# == 0)); then
 		bprint.die "The name of an executable must be passed"
 	fi
 
-	if ((${#args[@]} > 1)); then
-		bprint.die "The only argument must be the executable name"
-	fi
+	local bin_name="$0"
+	shift
 
 	# Look in current package
-	local bin_name="${args[0]}"
 	if util.get_toml_array "$BASALT_LOCAL_PROJECT_DIR/basalt.toml" 'binDirs'; then
 		for bin_dir in "${REPLIES[@]}"; do
 			for bin_file in "$BASALT_LOCAL_PROJECT_DIR/$bin_dir"/*; do
 				if [ -f "$bin_file" ] && [ -x "$bin_file" ]; then
 					util.deinit
+					bprint.info "Running $bin_file"
 					exec "$bin_file" "$@"
 				elif [ -f "$bin_file" ]; then
 					bprint.die "File '$bin_name' is found, but the package providing it has not made it executable"
@@ -47,6 +31,7 @@ do-run() {
 	local bin_file="$BASALT_LOCAL_PROJECT_DIR/.basalt/bin/$bin_name"
 	if [ -f "$bin_file" ] && [ -x "$bin_file" ]; then
 		util.deinit
+		bprint.info "Running $bin_file"
 		exec "$bin_file" "$@"
 	elif [ -f "$bin_file" ]; then
 		bprint.die "File '$bin_name' is found, but the package providing it has not made it executable"
