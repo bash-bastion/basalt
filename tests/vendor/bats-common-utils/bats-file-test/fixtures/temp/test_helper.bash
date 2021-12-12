4deb17e8f9a589300fd861c4e78420cd6ac3fc88
Edwin Kofler <edwin@kofler.dev>
2021-12-12 03:25:29 -0800

    chore: Upgrade version of `bats-all`
    Previous called `bats-common-util`, `bats-all` has been upgraded to the
    latest version (v4.2.0)

--- tests/vendor/bats-common-utils/bats-file-test/fixtures/temp/test_helper.bash
+++
@@ -1,5 +0,0 @@
-export TEST_MAIN_DIR="${BATS_TEST_DIRNAME}/../../.."
-export TEST_DEPS_DIR="${TEST_DEPS_DIR-${TEST_MAIN_DIR}/..}"
-
-# Load dependencies and library.
-load "${TEST_MAIN_DIR}/load.bash"
