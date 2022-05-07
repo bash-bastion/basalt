ef521a1465359f732301ee8a6447cbf2acb16685
Edwin Kofler <edwin@kofler.dev>
2022-05-06 17:21:18 -0700

    fix: Properly use error guards in Basalt init templates

--- pkg/share/templates/bare-app/tests/util/init.sh
+++ pkg/share/templates/bare-app/tests/util/init.sh
@@ -1,9 +1,9 @@
 # shellcheck shell=bash
 
 eval "$(basalt-package-init)" || exit
-basalt.package-init
+basalt.package-init || exit
 basalt.package-load
-# basalt.load 'github.com/hyperupcall/bats-all' 'load.bash' || exit
+# basalt.load 'github.com/hyperupcall/bats-all' 'load.bash'
 
 load './util/test_util.sh'
 
