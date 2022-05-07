c083015742abc82a40df7d082dc95fca78f54512
Edwin Kofler <edwin@kofler.dev>
2022-05-07 11:09:27 -0700

    chore(bakefile): Fix

--- Bakefile.sh
+++ Bakefile.sh
@@ -4,6 +4,17 @@ task.test() {
 	bats ./tests
 }
 
-task.update-subtree() {
+task.subtree-info() {
+	local vendor_dir="${1:-./pkg/vendor/bash-core}"
+	bake.info "vendor_dir: $vendor_dir"
+
+	local commit=
+	commit=$(git log --pretty=format:'%H' -- "$vendor_dir")
+	git show -s --format=%B "$(git log --pretty='%P' -n1 "$commit" | awk '{ print $2 }' )"
+}
+
+task.subtree-update() {
 	git subtree --squash -P tests/vendor/bats-all pull 'https://github.com/hyperupcall/bats-all' "$1"
 }
+
+
